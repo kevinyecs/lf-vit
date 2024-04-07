0792457936209d6bff283a7b250a67df3c5e553d
Armand <72499921+brandnewchoppa@users.noreply.github.com>
2024-04-07 19:00:58 +0200

    Update utils.py

--- lf_vit_pytorch/utils.py
+++ lf_vit_pytorch/utils.py
@@ -2,19 +2,51 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from typing import Optional, List
-
 class PrepareForTrainer(nn.Module):
     """
     To make the model compatible with HuggingFace's Trainer the model should
-    produce a dictionary containing the 'logits' and 'loss'.
+    produce a dictionary containing the 'logits' and 'loss'
 
     This class implements a custom loss computation and forward pass.
-    Also doesn't require any 'label' or 'attention_mask' columns, if any of
-    them passed as argument the model will ignore it.
+    Also doesn't require to pass the down scaled images as the model wants,
+    the resize is happens here in the forward pass with interpolation.
+
+    For interpolation we use 'torch.nn.functional.interpolate' and the mode
+    can be changed through 'model.interpolate_mode' attribute.
+
+    Args:
+        model (nn.Module):
+            LF-ViT model
+
+        classification_type (string):
+            Defines the classification type which can be (binary, multiclass)
+
+        interpolate_mode (string):
+            Defines the interpolation mode
+            For further detailes check:
+            https://pytorch.org/docs/stable/generated/torch.nn.functional.interpolate.html
     """
-    
-    def __init__(self, model):
+
+    def __init__(self, model, classification_type: string):
         super().__init__()
         self.model = model
-        self.loss_fn = nn.CrossEntropyLoss()
+        self.classification_type = classification_type
+        self.interpolate_mode = 'nearest'
+
+        if classification_type == 'binary':
+            self.loss_fn = nn.BCEWithLogitsLoss()
+        elif classification_type == 'multiclass':
+            self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self,
+                pixel_values: torch.Tensor,
+                labels: torch.Tensor):
+        
+        scale_ratio = self.model.model_config.downscale_ratio
+        scaled_shape = (pixel_values.shape[-2] // scale_ratio, pixel_values.shape[-1] // scale_ratio)
+        scaled_pixel_values = F.interpolate(pixel_values, size = scaled_shape, mode = self.interpolate_mode)
+
+        logits = self.model(pixel_values, scaled_pixel_values)
+        return {
+            'logits': logits,
+        }
