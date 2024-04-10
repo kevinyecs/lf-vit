6395b6b560bf0e1b41483a204cfa03ec36137803
Armand <72499921+brandnewchoppa@users.noreply.github.com>
2024-04-10 16:49:54 +0200

    Update utils.py

--- lf_vit_pytorch/utils.py
+++ lf_vit_pytorch/utils.py
@@ -47,13 +47,13 @@ class PrepareForTrainer(nn.Module):
         elif self.classification_type == 'multiclass':
             return self.loss_fn(logits.view(-1, self.model.n_labels), labels.view(-1))
 
-     def print_trainable_parameters(self):
-            trainable_params, all_params = 0, 0
-            for _, param in self.model.named_parameters():
-                all_params += param.numel()
-                if param.requires_grad:
-                    trainable_params += param.numel()
-            print(f'trainable params: {trainable_params:,} | all params: {all_params:,} | trainable: {100 * trainable_params / all_params}%')
+    def print_trainable_parameters(self):
+        trainable_params, all_params = 0, 0
+        for _, param in self.model.named_parameters():
+            all_params += param.numel()
+            if param.requires_grad:
+                trainable_params += param.numel()
+        print(f'trainable params: {trainable_params:,} | all params: {all_params:,} | trainable: {100 * trainable_params / all_params}%')
     
     def forward(self,
                 pixel_values: torch.Tensor,
